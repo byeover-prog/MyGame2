035eef851be389000042c244dcf8852727b92127
강세환(플밍4기) <ghks5658@gmail.com>
2026-04-11 23:21:55 +0900

    케릭터 추적 시네머신 구현

---
+++ Assets/_Game/Scripts/Camera_Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("카메라 목록")]
+    [SerializeField] private CinemachineCamera _playerCamera;
+    
+    // 타겟 목록
+    private Transform _player;
+    void Start()
+    {
+        LookAtPlayer();
+    }
+
+    void OnEnable()
+    {
+        LookAtPlayer();
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+    
+    // 버츄얼 카메라의 타겟을 설정하는 함수
+    private void SetFollowTarget(CinemachineCamera vcam, Transform target)
+    {
+        vcam.Follow = target;
+        vcam.ForceCameraPosition(target.position, target.rotation);
+    }
+    
+    // 플레이어 탐색 코루틴
+    private IEnumerator LookAtPlayer()
+    {
+        while (_player == null)
+        {
+            _player = GameObject.FindGameObjectWithTag("Player").transform;
+            yield return null;
+        }
+        SetFollowTarget(_playerCamera, _player);
+    }
+}
